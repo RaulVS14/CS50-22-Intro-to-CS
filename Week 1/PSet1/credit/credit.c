7e100bf0ea3bef23df5700d40751ce9ed540e02b
raulm <raul.muser@stacc.ee>
2022-02-19 00:17:09 +0200

    Update credit.c with final solution

--- Week 1/PSet1/credit/credit.c
+++ Week 1/PSet1/credit/credit.c
@@ -1,53 +1,84 @@
-#include <cs50.h>
+#include "../../../libcs50-10.1.1/src/cs50.h"
 #include <stdio.h>
+#include <math.h>
 
-bool checkCardChecksum(long cardNumber);
-
-int main(void) {
-
-    // Print card
-    // AMEX: 15 digit, 34 || 37
-    // MASTERCARD: 16 digit, starts with 51-55
-    // VISA: 13 || 16, starts with 4
-    // Get card number from input
+int main(void)
+{
     long cardNumber = 0;
-    do {
+    do
+    {
         cardNumber = get_long("Number: ");
-    } while (cardNumber < 1);
+        printf("%l", get_long("Number: "));
+    }
+    while (cardNumber < 1);
     // Calculate checksum
-    bool checkSum = checkCardChecksum(cardNumber);
-    // Check length and starting digits
-    printf("%s", checkSum ? "true" : "false");
-
-}
-
-bool checkCardChecksum(long cardNumber) {
     int productSum = 0;
     int nonProductSum = 0;
     long workingNumber = cardNumber;
-    int i = 1;
-    while (workingNumber > 0) {
+    int i = 0;
+    string result = "INVALID";
+    while (workingNumber > 0)
+    {
         int currentNumber = workingNumber % 10;
         workingNumber = workingNumber / 10;
-        if (i % 2 == 0) {
+        if (i % 2 == 1)
+        {
             // Multiply every other digit starting from second-to-last digit with 2
             int product = 2 * currentNumber;
-            if (product / 10 == 0) {
+            if (product / 10 == 0)
+            {
                 productSum += product;
-            } else {
+            }
+            else
+            {
                 productSum += product % 10;
                 productSum += (product / 10) % 10;
             }
             // Add products together
-        } else {
+        }
+        else
+        {
             // Sum digits that weren't multiplied
             nonProductSum += currentNumber;
         }
         i++;
     }
+
     // Add sum together
-    printf("%i, %i", productSum, nonProductSum);
     int numbersSum = productSum + nonProductSum;
     // Check if last digit is 0, if it is, it is valid checksum
-    return numbersSum % 10 == 0;
+    // Check length and starting digits
+
+    // AMEX: 15 digit, 34 || 37
+    // MASTERCARD: 16 digit, starts with 51-55
+    // VISA: 13 || 16, starts with 4
+    // Get card number from input
+    if (numbersSum % 10 == 0)
+    {
+        // Calculate powers for division to find first and second number
+        long lengthFirst = pow(10, (i - 1));
+        long lengthSecond = pow(10, (i - 2));
+        // Calculate first and second number
+        int firstNr = cardNumber / lengthFirst;
+        int secondNr = (cardNumber / lengthSecond) % 10;
+        if (i == 15 && firstNr == 3 && (secondNr == 4 || secondNr == 7))
+        {
+            // CHECK FOR AMEX
+            result = "AMEX";
+        }
+        else if ((i == 16 || i == 13) && firstNr == 4)
+        {
+            // CHECK FOR VISA
+            result = "VISA";
+        }
+        else if (i == 16 && firstNr == 5 && (secondNr >= 1 && secondNr <= 5))
+        {
+            // CHECK FOR MASTERCARD
+            result = "MASTERCARD";
+        }
+    }
+
+    // Print card
+    printf("%s\n", result);
+
 }
